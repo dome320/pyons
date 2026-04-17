e8ae6bbdd8ce34b0f7aaa1de03a3db3c0c6f0891
Domenic <dome32007@gmail.com>
2026-04-17 13:32:31 -0400

    implment Individual Class

--- pyons.py
+++ pyons.py
@@ -1,1 +1,18 @@
+from abc import ABC, abstractmethod
 
+
+class Individual(ABC):
+
+    @abstractmethod
+    def to_genome(self) -> list:
+        pass
+    
+    @abstractmethod
+    def from_genome(self) -> 'Individual': 
+        pass
+
+    def to_dict(self) -> dict:
+        pass
+
+    def from_dict(self) -> 'Individual': 
+        pass
