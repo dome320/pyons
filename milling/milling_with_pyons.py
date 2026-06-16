623e60003da2f086d5d606b3c1cbafd262633acf
michabay05 <michabay05@gmail.com>
2026-06-16 10:05:57 -0400

    michael's suggestion

--- milling/milling_with_pyons.py
+++ milling/milling_with_pyons.py
@@ -1,5 +1,7 @@
-import numpy as np 
+import numpy as np
 from world_setup import simulate
+
+from swarmsim.util.processing.multicoreprocessing import process_map
 
 step = 0.01
 
@@ -11,14 +13,28 @@ W_MAX: float = 0.60
 W_MIN: float = -0.60
 W_VALUES = np.arange(W_MIN, W_MAX + step, step)
 
-POP_COUNT: int = 100 
+POP_COUNT: int = 100
 MAX_GEN_COUNT: int = 10000
 DUP_RATE: float = 0.1
 MUT_RATE: float = 0.05
 
+# def fitness_individual(individual: list[float], target: float) -> float:
+def fitness_individual(config: tuple[np.array, float]) -> float:
+    individual, target = config
+    fitness = simulate(individual)
+    return target - fitness
+
+def fitness_mp(pop_matrix: np.array, target: float) -> np.array:
+    args = []
+    for indiv in pop_matrix:
+        args.append((indiv, target))
+
+    # args = [(indiv, target) for indiv in pop_matrix]
+
+    return np.array(process_map(fitness_individual, args))
 
 def update_fitness(pop_matrix: np.array, target: float) -> np.array:
-    fitness_values = [] 
+    fitness_values = []
 
     for l in pop_matrix:
         fitness = simulate(l)
@@ -26,7 +42,7 @@ def update_fitness(pop_matrix: np.array, target: float) -> np.array:
 
     return np.array(fitness_values)
 
-        
+
 
 def main():
     rng = np.random.default_rng(99)
@@ -42,20 +58,21 @@ def main():
     try:
         while best_fit > 0 and epoch < MAX_GEN_COUNT:
             print(f"[ep. {epoch}] '{best_genome}'; fitness = {best_fit}")
-            
-            fitness = update_fitness(pop_matrix, target)
+
+            # fitness = update_fitness(pop_matrix, target)
+            fitness = fitness_mp(pop_matrix, target)
 
             indices = np.argsort(fitness)
             pop_matrix = pop_matrix[indices]
-            fitness = fitness[indices] 
+            fitness = fitness[indices]
             if fitness[0] < best_fit:
                 best_genome = pop_matrix[0]
                 best_fit = fitness[0]
-            
+
             new_pop_matrix = np.zeros_like(pop_matrix)
 
-            dup_count = int(DUP_RATE * POP_COUNT) 
-            new_pop_matrix[:dup_count] = pop_matrix[:dup_count] 
+            dup_count = int(DUP_RATE * POP_COUNT)
+            new_pop_matrix[:dup_count] = pop_matrix[:dup_count]
 
             for i in range(dup_count, POP_COUNT):
                 p1, p2 = rng.choice(pop_matrix[:dup_count + 10], size=2, replace=False)
@@ -82,10 +99,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
--- milling/world_setup.py
+++ milling/world_setup.py
@@ -4,7 +4,6 @@ from swarmsim.world.RectangularWorld import RectangularWorld, RectangularWorldCo
 from swarmsim.agent.MazeAgent import MazeAgent, MazeAgentConfig
 from swarmsim.world.simulate import main as sim
 from swarmsim.agent.control.Controller import AbstractController
-from swarmsim.world.spawners.DonutSpawner import DonutAgentSpawner
 from swarmsim.world.spawners.AgentSpawner import PointAgentSpawner
 from swarmsim.metrics.Circliness import Circliness
 from swarmsim.sensors.BinaryFOVSensor import BinaryFOVSensor
@@ -15,19 +14,21 @@ class milling(AbstractController):
         super().__init__(parent)
         self.speeds=speeds
 
-    
     def get_actions(self, agent):
         if agent.agent_in_sight:
             return self.speeds[0], self.speeds[1]
         else:
-            return self.speeds[2], self.speeds[3] 
-    
+            return self.speeds[2], self.speeds[3]
+
 
 def simulate(speeds: list, steps: int = 1000, show=False) -> float:
-    world_config = RectangularWorldConfig(size=[10,10],time_step=1/40)
+    world_config = RectangularWorldConfig(
+        size=[10,10],time_step=1/40,
+        # stop_at=steps
+    )
     world = RectangularWorld(world_config)
 
-    agent_config = MazeAgentConfig(position=(5,5), agent_radius=0.1) 
+    agent_config = MazeAgentConfig(position=(5,5), agent_radius=0.1)
     agent = MazeAgent(agent_config, world)
 
     sensor = BinaryFOVSensor(agent=agent,theta=0.45,distance=2.0,false_positive=0.0,false_negative=0.0,show=False)
@@ -41,8 +42,8 @@ def simulate(speeds: list, steps: int = 1000, show=False) -> float:
     # Donut spawner doesn't work / is outdated!!
 
     spawner = PointAgentSpawner(world, n=6, facing="away",avoid_overlap=True, agent=agent, mode="oneshot")
-    
-    world.spawners.append(spawner) 
+
+    world.spawners.append(spawner)
 
     if not show:
         circliness = Circliness(history=steps)
@@ -54,5 +55,13 @@ def simulate(speeds: list, steps: int = 1000, show=False) -> float:
             world.step()
 
         return float(circliness.average)
-    
-    sim(world) 
+
+    sim(world)
+
+
+    # world.metrics = [Circliness(history=steps)]
+    # sim(
+    #     world,
+    #     show_gui=show,
+    # )
+    # return float(world.metrics[0].average)
